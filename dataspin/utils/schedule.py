5ef9d34058d6f79591ee1436e68e73e0ad26391e
ZhuoWei <zeaphoo@qq.com>
2022-02-17 20:12:44 +0800

    WIP: schedule parser

--- dataspin/utils/schedule.py
+++ dataspin/utils/schedule.py
@@ -4,20 +4,34 @@ from parsy import seq
 from datetime import date
 
 def _create_parser():
+    def _to_int(s):
+        if not s:
+            return 1
+        return int(s)
     space = parsy.string(" ").at_least(1)
     every = parsy.string("every")
     number = parsy.regex('[0-9]+').map(int)
-    year = (parsy.string('year') | parsy.string('y'))
-    month = (parsy.string('month') | parsy.string('mon') | parsy.string('M'))
+    year = (parsy.string('year').result('y') | parsy.string('y'))
+    month = (parsy.string('month').result('mon') | parsy.string('mon') | parsy.string('M').result('mon'))
     day = (parsy.string('day').result('d') | parsy.string('d'))
-    number_day = seq(number.optional(), space.optional() >> day)
+    week = (parsy.string('week').result('w') | parsy.string('w'))
+    hour = (parsy.string('hour').result('h') | parsy.string('h'))
+    minute = (parsy.string('minute').result('m') | parsy.string('m'))
+    second = (parsy.string('second').result('s') | parsy.string('s'))
+
+    number_month = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> month) 
+    number_week = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> week)
+    number_day = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> day)
+    number_hour = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> hour)
+    number_second = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> second)
     
-    week = (parsy.string('week') | parsy.string('w'))
-    hour = (parsy.string('hour') | parsy.string('h'))
-    minute = (parsy.string('minute') | parsy.string('m'))
-    second = (parsy.string('second') | parsy.string('s'))
+    
     at = parsy.string('at')
-    return seq(every << space, number_day)
+    day_time = (seq(number, hour, number, minute, number, second) | seq(number, hour, number, minute)
+                | seq(number, minute, number, second) | seq(number, hour) | seq(number, minute) | seq(number, second))
+    at_statement = seq(space >> at, space >> day_time)
+    every_statement = seq(every << space, number_month | number_week | number_day | number_hour | number_second)
+    return every_statement + at_statement | every_statement
 
 scheduler_parser = _create_parser()
 
--- tests/test_utils/test_schedule.py
+++ tests/test_utils/test_schedule.py
@@ -6,5 +6,14 @@ def test_parse_simple_1():
     assert scheduler_parser.parse('every 1d') == ['every', [1, 'd']]
     assert scheduler_parser.parse('every 1day') == ['every', [1, 'd']]
     assert scheduler_parser.parse('every 1 day') == ['every', [1, 'd']]
+    assert scheduler_parser.parse('every day') == ['every', [1, 'd']]
+    assert scheduler_parser.parse('every 2 day') == ['every', [2, 'd']]
+    assert scheduler_parser.parse('every 2day') == ['every', [2, 'd']]
     with pytest.raises(ParseError):
         scheduler_parser.parse('every1d')
+
+def test_parse_simple_2():
+    assert scheduler_parser.parse('every 1d at 12h') == ['every', [1, 'd'], 'at', [12, 'h']]
+    assert scheduler_parser.parse('every 1d at 12h12m') == ['every', [1, 'd'], 'at', [12, 'h', 12, 'm']]
+    assert scheduler_parser.parse('every 2d at 12h2m3s') == ['every', [2, 'd'], 'at', [12, 'h', 2, 'm', 3, 's']]
+    assert scheduler_parser.parse('every hour at 2m3s') == ['every', [1, 'h'], 'at', [2, 'm', 3, 's']]
