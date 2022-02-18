a153bd2d66e10f5ef02615a08bc99b0ac67bc267
ZhuoWei <zeaphoo@qq.com>
2022-02-18 15:40:47 +0800

    WIP: schedule expression parser

--- dataspin/utils/schedule.py
+++ dataspin/utils/schedule.py
@@ -1,39 +1,78 @@
 
 import parsy
 from parsy import seq
-from datetime import date
+from datetime import date, timedelta
+'''
+every day at 12h30m
+every week day 7 at 8h:30m
+every month day 1 
+every week day 5 
+every week day 1 at 8h:30m
+every month day 5 at 8h:50m
+'''
+
+class DynamicTimedelta:
+    def __init__(self, months=0):
+        self.months = months
 
 def _create_parser():
     def _to_int(s):
         if not s:
             return 1
         return int(s)
+    
+    def _month_timedelta(months=1):
+        return DynamicTimedelta(months=months)
+    
+    def _day_timedelta(day_list):
+        if isinstance(day_list, (list, tuple)):
+            return map(lambda x: timedelta(days=x-1), day_list)
+        return [timedelta(days=day_list-1)]
+
     space = parsy.string(" ").at_least(1)
     every = parsy.string("every")
     number = parsy.regex('[0-9]+').map(int)
-    year = (parsy.string('year').result('y') | parsy.string('y'))
-    month = (parsy.string('month').result('mon') | parsy.string('mon') | parsy.string('M').result('mon'))
-    day = (parsy.string('day').result('d') | parsy.string('d'))
-    week = (parsy.string('week').result('w') | parsy.string('w'))
-    hour = (parsy.string('hour').result('h') | parsy.string('h'))
-    minute = (parsy.string('minute').result('m') | parsy.string('m'))
-    second = (parsy.string('second').result('s') | parsy.string('s'))
 
-    number_month = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> month) 
-    number_week = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> week)
-    number_day = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> day)
-    number_hour = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> hour)
-    number_second = seq(parsy.regex('[0-9]*').map(_to_int), space.optional() >> second)
+    number_list = (seq(number) + (parsy.string(",") >> number).many()).combine(_day_timedelta)
+
+    month = (parsy.string('months').result('mon') | parsy.string('month').result('mon'))
+    week = (parsy.string('weeks').result('w') | parsy.string('week').result('w') )
+    day = (parsy.string('days').result('d') | parsy.string('day').result('d') )
+    hour = (parsy.string('hours').result('h') | parsy.string('hour').result('h') )
+    minute = (parsy.string('minutes').result('m') | parsy.string('minute').result('m'))
+    second = (parsy.string('seconds').result('s') | parsy.string('second').result('s'))
+
+    h = parsy.string('h')
+    m = parsy.string('m')
+    s = parsy.string('s')
+
+    number_month = seq(months =parsy.regex('[0-9]*').map(_to_int), _t = space.optional() >> month).combine_dict(_month_timedelta)
+    number_week = seq(weeks =parsy.regex('[0-9]*').map(_to_int), _t = space.optional() >> week).combine_dict(timedelta)
+    number_day = seq(days = parsy.regex('[0-9]*').map(_to_int), _t = space.optional() >> day).combine_dict(timedelta)
+    number_hour = seq(hours = parsy.regex('[0-9]*').map(_to_int), _t = space.optional() >> hour).combine_dict(timedelta)
+    number_minute = seq(minutes = parsy.regex('[0-9]*').map(_to_int), _t = space.optional() >> minute).combine_dict(timedelta)
+    number_second = seq(seconds = parsy.regex('[0-9]*').map(_to_int), _t = space.optional() >> second).combine_dict(timedelta)
     
     
     at = parsy.string('at')
-    day_time = (seq(number, hour, number, minute, number, second) | seq(number, hour, number, minute)
-                | seq(number, minute, number, second) | seq(number, hour) | seq(number, minute) | seq(number, second))
+    colon = parsy.string(':')
+    hour_time = seq(hours = number, _t = h).combine_dict(timedelta)
+    minute_time = seq(minutes = number, _t = m).combine_dict(timedelta)
+    second_time = seq(seconds = number, _t = s).combine_dict(timedelta)
+    day_time = ((hour_time + (colon.optional() >> minute_time) + (colon.optional() >> second_time))
+                 | (hour_time + (colon.optional() >> minute_time)) 
+                 | (minute_time + (colon.optional() >> second_time))
+                 | hour_time | minute_time | second_time )
     at_statement = seq(space >> at, space >> day_time)
-    every_statement = seq(every << space, number_month | number_week | number_day | number_hour | number_second)
+    day_statement = seq(parsy.string("day") << space, number_list)
+    every_statement = (
+                    seq(every << space, number_day | number_hour | number_minute | number_second) 
+                    | seq(every << space, (number_week | number_month) << space) + day_statement
+                    )
     return every_statement + at_statement | every_statement
 
 scheduler_parser = _create_parser()
 
 def parse_schedule_string(sched_str):
     pass
+
